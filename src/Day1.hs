94fdd4cf2748246a9e6d0a71a6bf2ea04f945a2d
Jason Fields <jasonfields4@gmail.com>
2018-12-02 22:38:28 -0500

    Day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,4 +1,20 @@
 module Day1 where
 
+import qualified Data.Set as Set
+
+parseInt :: String -> Int
+parseInt ('+':num) = read num
+parseInt num = read num
+
+firstRepeatedFreq :: Int -> Set.Set Int -> [Int] -> Int
+firstRepeatedFreq currFreq freqs (delta:rest) =
+    if newFreq `Set.member` freqs
+    then newFreq
+    else firstRepeatedFreq newFreq (newFreq `Set.insert` freqs) rest 
+    where newFreq = currFreq + delta
+
 run :: IO ()
-run = putStrLn "Day 1 not implemented"
+run = do
+    contents <- readFile "data/Day1.input"
+    let result = firstRepeatedFreq 0 Set.empty . cycle . map parseInt $ lines contents
+    print result
