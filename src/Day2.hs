e972680e34f0db704c816649d0f9899aaf0df829
Jason Fields <jasonfields4@gmail.com>
2018-12-02 23:07:46 -0500

    Day 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,4 +1,36 @@
 module Day2 where
 
+import Data.List
+
+hasOccurrences :: Eq a => Int -> [a] -> Bool
+hasOccurrences numOcc word =
+    any id [length (filter (== c) word) == numOcc | c <- (nub word)]
+
+checksum :: [String] -> Int
+checksum [] = 0
+checksum wordlist = 
+    twos * threes
+    where
+        twos = length $ filter (hasOccurrences 2) wordlist
+        threes = length $ filter (hasOccurrences 3) wordlist
+
+numDifferences :: Eq a => [a] -> [a] -> Int
+numDifferences xs ys =
+    length . filter id $ zipWith (/=) xs ys
+
+similarWords :: [String] -> [String]
+similarWords [] = []
+similarWords words =
+    [x | x <- words, y <- words, x `isSimilar` y]
+    where
+        x `isSimilar` y =
+            numDifferences x y == 1
+
 run :: IO ()
-run = putStrLn "Day 2 not implemented"
+run = do
+    contents <- readFile "data/Day2.input"
+    let wordlist = lines contents
+    putStr "Checksum:"
+    print $ checksum wordlist
+    putStr "Similar words:"
+    print $ similarWords wordlist
